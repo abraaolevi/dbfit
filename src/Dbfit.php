e96cd25379470eb364d976471543a39c2ba94f0d
Abraão Levi <abr4ao.levi@gmail.com>
2016-12-28 20:53:53 -0200

    docs and improvements at src/Dbfit.php

--- src/Dbfit.php
+++ src/Dbfit.php
@@ -5,12 +5,56 @@ namespace Dbfit;
 use Dbfit\Connection;
 use Dbfit\ConnectionManager;
 
+/**
+ * Dbfit
+ * 
+ * Responsible for facilitating day-to-day SQL queries
+ * 
+ * ### USAGE
+ * 
+ * Simple queries:
+ * 
+ * ```
+ * $db = new Dbfit($host, $user, $password, $database);
+ * $result = $db->query("SELECT * FROM users");
+ * ```
+ * 
+ * Transactions queries:
+ * ```
+ * $db->getConnection()->beginTransaction();
+ * $db->query($sql);
+ * $db->query($sql);
+ * $db->query($sql);
+ * $db->getConnection()->endTransaction();
+ * ```
+ * 
+ * You can cancel a transaction using `$db->getConnection()->cancelTransaction();`
+ * 
+ * @since 0.1
+ * @author Abraao Levi <https://github.com/abraaolevi>
+ */
 class Dbfit
 {
 
+    /**
+     * Database connection
+     * @var \Dbfit\Connection
+     */
     private $conn;
+
+    /**
+     * Statement query
+     * @var \Dbfit\Statement 
+     */
     private $stmt;
 
+    /**
+     * Constructor class
+     * @param string $host
+     * @param string $user
+     * @param string $password
+     * @param string $database
+     */
     public function __construct(string $host, string $user, string $password, string $database)
     {
         ConnectionManager::config("default", [
@@ -22,13 +66,71 @@ class Dbfit
 
         $this->conn = new Connection(ConnectionManager::get("default"));
     }
-    
-    public function query(string $query){
-        $this->stmt = $this->conn->prepareQuery($query);
-        if(!$this->stmt->execute()) {
+
+    /**
+     * Get result from SQL
+     * this method prepare, execute and get result set from string SQL
+     * 
+     * ### Usage
+     * 
+     * ```
+     * $result = $db->query("SELECT * FROM users");
+     * ```
+     * 
+     * or
+     * 
+     * ```
+     * $result = $db->query("INSERT INTO users (name, age, gender) VALUES (:name, :age, :gender)", [
+     *      ":name" => "John",
+     *      ":age" => 25,
+     *      ":gender" => "male"
+     * ]);
+     * ```
+     * 
+     * @param string $sql
+     * @param array $params
+     * @param bool $rawResponse
+     * @return array
+     * @throws \Exception
+     */
+    public function query(string $sql, array $params = [], bool $rawResponse = true): array
+    {
+        $this->stmt = $this->conn->prepareQuery($sql);
+
+        foreach ($params as $key => $value) {
+            $this->stmt->bind($key, $value);
+        }
+
+        if (!$this->stmt->execute()) {
             throw new \Exception("Fail to execute statement query");
         }
-        return $this->stmt->resultset();
+        
+        if($rawResponse){
+            return $this->stmt->resultset();
+        }
+        
+        return [
+            "result" => $this->stmt->resultset(),
+            "count" => $this->stmt->rowCount()
+        ];
+    }
+    
+    /**
+     * Return the Connection
+     * @return \Dbfit\Connection
+     */
+    public function getConnection(): Connection
+    {
+        return $this->conn;
+    }
+    
+    /**
+     * Return the Statement
+     * @return \Dbfit\Statement
+     */
+    public function getStatement(): Statement
+    {
+        return $this->stmt;
     }
     
 }
--- src/Statement.php
+++ src/Statement.php
@@ -30,53 +30,74 @@ class Statement
     }
     
     /**
-     * 
-     * @param mixed $param
-     * @param mixed $value
-     * @param mixed $type
+     * Bind values on statement
+     * @param string $param
+     * @param mixed $value based on `$type`
+     * @param mixed $type can be `null` for any type or `\PDO::PARAM_INT`, `\PDO::PARAM_BOOL`, `\PDO::PARAM_NULL` or `\PDO::PARAM_STR`
+     * @return bool
      */
-    public function bind($param, $value, $type = null)
+    public function bind(string $param, $value, $type = null): bool
     {
         if (is_null($type)) {
             switch (true) {
                 case is_int($value):
-                    $type = PDO::PARAM_INT;
+                    $type = \PDO::PARAM_INT;
                     break;
                 case is_bool($value):
-                    $type = PDO::PARAM_BOOL;
+                    $type = \PDO::PARAM_BOOL;
                     break;
                 case is_null($value):
-                    $type = PDO::PARAM_NULL;
+                    $type = \PDO::PARAM_NULL;
                     break;
                 default:
-                    $type = PDO::PARAM_STR;
+                    $type = \PDO::PARAM_STR;
             }
         }
-        $this->stmt->bindValue($param, $value, $type);
+        return $this->stmt->bindValue($param, $value, $type);
     }
     
-    public function execute()
+    /**
+     * Executes a prepared statement
+     * @return bool
+     */
+    public function execute(): bool
     {
         return $this->stmt->execute();
     }
 
-    public function resultset()
+    /**
+     * Returns an array containing all of the result set rows
+     * @return array
+     */
+    public function resultset(): array
     {
         $this->execute();
         return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function single()
+    /**
+     * Return single first record from result set
+     * @return array
+     */
+    public function single(): array
     {
         $this->execute();
         return $this->stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function rowCount()
+    /**
+     * Returns the number of rows affected by the last SQL statement
+     * @return int
+     */
+    public function rowCount(): int
     {
         return $this->stmt->rowCount();
     }
     
+    /**
+     * Dump an SQL prepared command
+     * @return type
+     */
     public function debug()
     {
         return $this->stmt->debugDumpParams();
